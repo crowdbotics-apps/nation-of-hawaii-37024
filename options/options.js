8334cdbc1a10e4d66d611943c004de7886cc621e
Crowdbotics <team@crowdbotics.com>
2022-09-09 16:47:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "nation_of_hawaii_37024",
+  url: "https://nation_of_hawaii_37024.botics.co",
+  api: "https://nation_of_hawaii_37024.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
